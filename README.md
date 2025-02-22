502e105d4a24ee2ec4fdbd656438eaca981230aa
Rob <r_mcinnis@solidice.com>
2025-02-22 04:22:50 -0500

    initial revision

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # hello_cpp
 c++ version of basic hello world w/ cmake 
+
+```
+clone the repo
+cd hello_cpp
+mkdir build
+cd build
+cmake ..
+make
+./hello
+```
+
---
+++ src/main_hello.cpp
@@ -0,0 +1,15 @@
+/*
+  main_hello.cpp 
+  basic hello world with cmake 
+
+  developer   tiny@solidice.com
+  date        22 feb 2025
+*/
+#include <stdio.h>
+
+int main( int argc, const char *argv[] )
+{
+  printf( "Hello world! \n" ) ;
+  return 0 ;
+} // :: main
+
